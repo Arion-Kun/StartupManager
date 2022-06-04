54925105da5c6a93f2e26f788faca2d3f995e8b1
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-06-05 01:52:00 +0200

    TaskScheduler Initial Implementation

---
+++ src/StartupManager/Utilities/Extensions/TaskSchedulerEx.cs
@@ -0,0 +1,16 @@
+﻿namespace Dawn.Apps.StartupManager.Extensions;
+
+using System.Security.Principal;
+using Microsoft.Win32.TaskScheduler;
+
+internal static class TaskSchedulerEx
+{
+    internal static LogonTrigger AsCurrentUser(this LogonTrigger trigger)
+    {
+        trigger.UserId = WindowsIdentity.GetCurrent().Name;
+        return trigger;
+    }
+
+    internal static Task RegisterAsCurrentUser(this TaskFolder folder, string path, TaskDefinition definition, TaskCreation creationType = TaskCreation.CreateOrUpdate) 
+        => folder.RegisterTaskDefinition(path, definition, creationType, WindowsIdentity.GetCurrent().Name, TaskService.Instance.UserPassword, TaskLogonType.InteractiveToken);
+}
