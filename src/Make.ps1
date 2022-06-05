d398184f791a3dd10b14201ad06ba01515379809
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-06-05 03:02:21 +0200

    Update Make.ps1

--- src/Make.ps1
+++ src/Make.ps1
@@ -1,7 +1,22 @@
-# Minimum Script Requirement: Set-ExecutionPolicy RemoteSigned
+# Minimum Script Requirement: Set-ExecutionPolicy Bypass
 
 $NUGET_LINK = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
 $DOTNET_LINK = 'https://dot.net/v1/dotnet-install.ps1'
+$NET48DEVPACK_LINK = 'https://go.microsoft.com/fwlink/?linkid=2088517'
+
+Write-Output "The project needs the .Net 4.8 Developer Pack to successfully build"
+Write-Output "If you are certain this is installed you should say 'No' to the installation prompt"
+Write-Output "Would you like to download and install the '.Net 4.8 Developer Pack' now? y/n"
+
+$downloadNet48 = Read-Host
+if ($downloadNet48 -eq "y")
+{
+    Write-Output "This may take some time (142mb)."
+    Invoke-WebRequest $NET48DEVPACK_LINK -O ndp48-devpack-enu.exe
+    ./ndp48-devpack-enu.exe /install /passive
+    Write-Output "Would you like to continue?"
+    Read-Host    
+}
 
 # Check if Nuget.exe is in this working directory.
 if (!(Test-Path -Path Nuget.exe)) 
@@ -31,6 +46,7 @@ function Start-Cleanup
     #Trailing Artifacts
     Remove-If-Present 'dotnet-install.ps1'
     Remove-If-Present 'Nuget.exe'
+    Remove-If-Present 'ndp48-devpack-enu.exe'
 }
 
 if (!(IsDotnetPresent))
@@ -40,26 +56,17 @@ if (!(IsDotnetPresent))
     if (!(Test-Path -Path dotnet-install.ps1)) 
     {
         Invoke-WebRequest $DOTNET_LINK -O dotnet-install.ps1
-        Write-Output "The install file has been downloaded, would you like to install now? y/n"
+        Write-Output "The install file has been downloaded."
     }
     else 
     {
-        Write-Output "The install file has been detected, would you like to install now? y/n"
+        Write-Output "The install file has been detected."
     }
     
-    $resp = Read-Host
-    if ($resp -eq "y")
-    {
-        Write-Output "Installing Please wait..."
-        Write-Output "This might take a while!"
-        Write-Output "#########################"
-        ./dotnet-install.ps1
-    }
-    else 
-    {
-        Start-Cleanup
-        Exit-PSHostProcess
-    }
+    Write-Output "Installing Please wait..."
+    Write-Output "This might take a while!"
+    Write-Output "#########################"
+    ./dotnet-install.ps1
 }
 dotnet build -c Release
 Start-Cleanup
